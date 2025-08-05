dfcc979f8e289ddea4fefd12177918900d485b6b
Yash-11111 <yashkesarwani8040@gmail.com>
2025-08-05 11:18:11 +0530

    initial commit

--- div.java
+++ div.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Division {
+public class div {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter numerator: ");
