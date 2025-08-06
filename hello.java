d3214917eab3d9294536ce40e2a94d1d2a8e05a6
Ashutosh Patel <ashutoshp141@gmail.com>
2025-08-06 11:06:00 +0530

    commit with conflicts

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Namaste and wadakam friends , Welcome to our Git Repository");
+        System.out.println("Namaste, Welcome to our Git Repository");
         System.out.println("");
     }
 }
